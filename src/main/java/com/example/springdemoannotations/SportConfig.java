4dfa14059a22fb635a9b9074e8d217b6691132fa
Elia Manara <elia.manara@aiknow.io>
2020-10-09 17:49:25 +0200

    Java configuration

---
+++ src/main/java/com/example/springdemoannotations/SportConfig.java
@@ -0,0 +1,10 @@
+package com.example.springdemoannotations;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.springdemoannotations")
+public class SportConfig {
+}
